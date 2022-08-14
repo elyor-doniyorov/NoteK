eaf8677268adb7de22a5119044ccb36369aa405c
Elyor Doniyorov <doniyorovelyor@gmail.com>
2022-08-14 14:52:16 +0500

    Add pagination

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header';
-import Note from './components/Note';
 import UserInput from './components/UserInput';
 import { useState, useEffect } from 'react';
 import NotesList from './components/NotesList';
+import { Pagination } from './components/Pagination';
 
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [notesPerPage, setNotesPerPage] = useState(6);
 
   const savedNotes = localStorage.getItem("notes")
     ? JSON.parse(localStorage.getItem("notes"))
@@ -30,11 +34,23 @@ function App() {
       return [...preValue.filter((note, index) => index !== id)]
     });
   }
+
+  //Get current notes
+  const indexOfLastNote = currentPage * notesPerPage;
+  const indexOfFirstNote = indexOfLastNote - notesPerPage;
+  const currentNotes = notes.slice(indexOfFirstNote, indexOfLastNote);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
+
+
   return (
     <div>
       <Header />
       <UserInput onAdd={addNote} />
-      <NotesList notes={notes} handleDeleteNote={deleteNotes} handleAddNote={addNote} />
+      <NotesList notes={currentNotes} handleDeleteNote={deleteNotes} handleAddNote={addNote} />
+      <Pagination notesPerPage={notesPerPage} totalNotes={notes.length} paginate={paginate} />
     </div>
   );
 }
